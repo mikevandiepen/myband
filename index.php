90af2182c50ead98272f76b73ababfc70ccf86f1
Mike van Diepen <38211249+mikevandiepen@users.noreply.github.com>
2018-07-25 15:11:26 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,127 @@
+<?php include('views/header.php'); ?>
+
+<section>
+    <nav>
+        <ul>
+            <li><a href="">About</a></li>
+            <li><a href="">Albumns</a></li>
+            <li><a href="">Records</a></li>
+            <li><a href="">Shows</a></li>
+            <li><a href="">Upcomming</a></li>
+            <li><a href="" id="red">Buy tickets</a></li>
+        </ul>
+    </nav>
+</section>
+
+<section class="landing">
+    <h1>Post Malone</h1>
+    <img src="assets/img/overlay.png">
+</section>
+
+<section class="events textCenter">
+    <div class="container">
+        <h1>Upcoming Events</h1>
+
+        <div class="row">
+            <div class="eventList">
+                <div class="one-third columns item spacing">
+                    <h3>Thursday 24 May 2018</h3>
+                    <p>with 21 Savage and SOB x RBE </p>
+                    <p>Xfinity Center, Mansfield, MA, US</p>
+                </div>
+            </div>
+
+            <div class="eventList">
+                <div class="one-third columns item spacing">
+                    <h3>Test</h3>
+                    <p>Date</p>
+                    <p>Other following information about the upcomming event</p>
+                </div>
+            </div>
+
+            <div class="eventList">
+                <div class="one-third columns item spacing">
+                    <h3>Test</h3>
+                    <p>Date</p>
+                    <p>Other following information about the upcomming event</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="stoney">
+    <div class="three columns">
+        <div class="covers" id="stoney"></div>
+        <div class="albumInfo textLeft">
+            <h3>Stoney</h3>
+            <p>
+                Stoney is the debut studio album by American rapper Post Malone. It was released on December 9, 2016, by Republic Records. The album features guest appearances from Justin Bieber, Kehlani, Quavo and 2 Chainz. The production on the album was handled by Metro Boomin, Vinylz, Frank Dukes, Illangelo, Charlie Handsome, Rex Kudo and Foreign Teck, among others
+            </p>
+        </div>
+    </div>
+
+    <div class="seven columns songs">
+        <h3 class="textCenter v-offsetTwo">Songs on the album</h3>
+
+        <ul class="four columns offsetOne v-offsetOne">
+            <li>Broken Whiskey Glass</li>
+            <li>Big Lie</li>
+            <li>Deja Vu</li>
+            <li>No Option</li>
+            <li>Cold</li>
+            <li>White Iverson</li>
+            <li>I fall Apart</li>
+            <li>Patient</li>
+            <li>Go Flex</li>
+        </ul>
+        <ul class="four columns v-offsetOne">
+            <li>Feel</li>
+            <li>Too Young</li>
+            <li>Congratulations</li>
+            <li>Up There</li>
+            <li>Yours Truly, Austin Post</li>
+            <li>Leave</li>
+            <li>Hit This Hard</li>
+            <li>Money Made Me Do It</li>
+            <li>Feeling Whitney</li>
+        </ul>
+    </div>
+</section>
+
+<section class="beerbongs">
+    <div class="three columns">
+        <div class="covers" id="beerbongs"></div>
+        <div class="albumInfo textLeft">
+            <h3>Beerbongs and Bentleys</h3>
+            <p>Beerbongs & Bentleys (stylized in lowercase letters) is the second studio album by American rapper Post Malone, released by Republic Records on April 27, 2018. The album features guest appearances from Swae Lee, 21 Savage, Ty Dolla Sign, Nicki Minaj, G-Eazy and YG. The album was supported by four singles: "Rockstar", "Candy Paint", "Psycho" and "Ball for Me".</p>
+        </div>
+    </div>
+
+    <div class="seven columns songs">
+        <h3 class="textCenter v-offsetTwo">Songs on the album</h3>
+
+        <ul class="four columns offsetOne v-offsetOne">
+            <li>Paranoid</li>
+            <li>Spoil my Night</li>
+            <li>Rich & Sad</li>
+            <li>Zack and Codeine</li>
+            <li>Takin' Shots</li>
+            <li>Rockstar</li>
+            <li>Over now</li>
+            <li>Psycho</li>
+            <li>Better Now</li>
+        </ul>
+        <ul class="four columns v-offsetOne">
+            <li>Ball for Me</li>
+            <li>Otherside</li>
+            <li>Stay</li>
+            <li>Blame It on Me</li>
+            <li>Same Bitches</li>
+            <li>Jonestown</li>
+            <li>92 Explorer</li>
+            <li>Candy Paint</li>
+            <li>Suger Wraith</li>
+        </ul>
+    </div>
+</section>
